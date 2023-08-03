740c7195e88cad4a219ec4c1f1363adf0dec0929
JisuEating <137197374+JisuEating@users.noreply.github.com>
2023-08-03 09:01:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function randomSelect() {
 }const items = [
     {
         name: "菜品1",
-        img:"img/3d0f4f8ca6ad653eb9f5bc53c6191d5.jpg",
-        description: "这是菜品1的描述"
+        img: "img/2e88a2068c9a6d0f163af027ac22e72.jpg",
+        description: "这是菜品1的描述吗"
     },
     {
         name: "菜品2",
         img: "img/3d0f4f8ca6ad653eb9f5bc53c6191d5.jpg",
-        description: "这是菜品1的描述"
+        description: "这是菜品2的描述"
     },
     {
         name: "菜品3",
@@ -36,22 +36,49 @@ function randomSelect() {
     {
         name: "麻辣烫",
         img: "img/新一A座川锦汇15r.jpg",
-        description: "新一A座川锦汇15r"
+        description: "新一A座（川锦汇）15r"
+    },
+    {
+        name: "蛋炒饭",
+        img: "img/2e88a2068c9a6d0f163af027ac22e72.jpg",
+        description: "新一A座（回味斋）7r"
     }
 ];
 
-const resultImg =document.querySelector(".result-img");
+const resultImg = document.querySelector(".result-img");
 const resultName = document.querySelector(".result-name");
 const drawButton = document.querySelector(".draw");
+//弹出框
+const lightbox = document.querySelector(".lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const lightboxClose = document.querySelector(".lightbox-close");
 
 drawButton.addEventListener("click", () => {
-    // 随机选择一个项目
-    const selectedItem = items[Math.floor(Math.random() * items.length)];
-    
-    // 更新抽奖结果
-    resultImg.src = selectedItem.img;
-    resultName.innerHTML = "<strong>" + selectedItem.name + "</strong>：<br>" + selectedItem.description;
-    
-    // 显示抽奖结果
-    document.querySelector(".result").style.display = "block";
+  // 随机选择一个项目
+  const selectedItem = items[Math.floor(Math.random() * items.length)];
+
+  // 更新抽奖结果
+  resultImg.src = selectedItem.img;
+  resultName.innerHTML = "<strong>" + selectedItem.name + "</strong>：<br>" + selectedItem.description;
+
+  // 显示抽奖结果
+  document.querySelector(".result").style.display = "block";
+});
+
+resultImg.addEventListener("click", () => {
+  // 显示弹出框
+  lightboxImg.src = resultImg.src;
+  lightbox.style.display = "block";
+});
+
+lightboxClose.addEventListener("click", () => {
+  // 隐藏弹出框
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", event => {
+  // 如果点击的是弹出框外部，隐藏弹出框
+  if (event.target === lightbox) {
+    lightbox.style.display = "none";
+  }
 });
